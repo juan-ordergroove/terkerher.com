769722e821bd82d5999780fdd5fa84ab7987892e
juanny_G <juanny.gee@gmail.com>
2013-12-23 13:56:29 -0500

    control animation configs via JS in order to allow randomization of all params

--- js/index.js
+++ js/index.js
@@ -7,8 +7,17 @@
     }
 
     function anim_settings(div) {
-        var l = Math.floor((Math.random()*win.outerWidth)+1);
+        var cb_1 = Math.random()*0.9 + 0.1;
+        var cb_2 = Math.random()*0.9 + 0.1;
+        var l = Math.floor((Math.random() * win.outerWidth) + 1);
+        var dur = Math.floor((Math.random() * 3) + 1);
+
         div.style.left = l+'px';
+        div.style.webkitAnimationName = 'rain';
+        div.style.webkitAnimationDuration = dur+'s';
+        div.style.webkitAnimationTimingFunction = 'cubic-bezier('+cb_1+','+cb_2+','+Math.abs(1-cb_1)+','+cb_2+')';
+        div.style.webkitAnimationDelay = '0s';
+        div.style.webkitAnimationIterationCount = 'infinite';
         return div;
     }
 
