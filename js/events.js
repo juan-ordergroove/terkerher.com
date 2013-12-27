abd24be4031b44ffc1860f3c028b9d83017ff775
juanny_G <juanny.gee@gmail.com>
2013-12-26 23:06:56 -0500

    add cross browser event manager; add intensity manager to allow for varying intensities of rate

---
+++ js/events.js
@@ -0,0 +1,34 @@
+eventManager = (function(win, doc) {
+    // add event cross browser
+    function addEvent(elem, event, fn) {
+        // avoid memory overhead of new anonymous 
+        // functions for every event handler that's installed
+        // by using local functions
+        function listenHandler(e) {
+            var ret = fn.apply(this, arguments);
+            if (ret === false) {
+                e.stopPropagation();
+                e.preventDefault();
+            }
+            return(ret);
+        }
+
+        function attachHandler() {
+            // set the this pointer same as addEventListener when fn is called
+            // and make sure the event is passed to the fn also so that works the same too
+            var ret = fn.call(elem, win.event);   
+            if (ret === false) {
+                win.event.returnValue = false;
+                win.event.cancelBubble = true;
+            }
+            return(ret);
+        }
+ 
+        if (elem.addEventListener) {
+            elem.addEventListener(event, listenHandler, false);
+        } else {
+            elem.attachEvent("on" + event, attachHandler);
+        }
+    }
+    return { addEvent: addEvent };
+}(window, document));
--- js/index.js
+++ js/index.js
@@ -1,8 +1,45 @@
+rainIntensity = (function(win, doc) {
+    var intensity = [];
+    function create_generator() {
+        var generator_id = setInterval(rainGenerator.init, 300);
+        intensity.push(generator_id);
+    }
+
+    function destroy_generator() {
+        var generator_id = intensity.pop();
+        clearInterval(generator_id);
+    }
+
+    function change_intensity(evt) {
+        var intensity_lvl = evt.target.value;
+        var curr_intensity = intensity.length;
+        var increase = (intensity_lvl > curr_intensity);
+        if (increase) {
+            while (intensity_lvl > curr_intensity) {
+                create_generator();
+                curr_intensity = intensity.length;
+            }
+        } else {
+            while (intensity_lvl < curr_intensity) {
+                destroy_generator();
+                curr_intensity = intensity.length;
+            }
+        }
+    }
+
+    function init() {
+        create_generator();
+        var intensity_s = document.getElementById('change_intensity');
+        eventManager.addEvent(intensity_s, 'change', change_intensity);
+    }
+    return { init: init };
+}(window, document));
+
 function x() {
     if (typeof css_support == "undefined") {
         document.write("Your browser does not support the required CSS properties");
         return;
     }
-    setInterval(rainGenerator.init, 300);
+    rainIntensity.init();
 }
 setTimeout(x, 500);
